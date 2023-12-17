3e9f4a7254d1a27746f93f32a985bbdc5b9a73ed
ilaydacaan <95634263+ilaydacaan@users.noreply.github.com>
2023-12-17 23:53:58 +0300

    Mail gönderme ve sayfa geçişleri güncellendi.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const PORT = 5000;
 const app = express();
+const bodyparser=require("body-parser");
+const nodemailer=require('nodemailer');
 
 app.use(express.static('public'));
 
@@ -14,14 +16,63 @@ app.get('/harita', (req, res) => {
   res.sendFile(__dirname + '/public/html/map.html');
 });
 
+
 // Satıcı sayfası URl uzantısı
 app.get('/satici', (req, res) => {
   res.sendFile(__dirname + '/public/html/saticiprofil.html');
 });
 
+app.get('/iletisim', (req, res) => {
+  res.sendFile(__dirname + '/public/html/anasayfa.html');
+});
 
 app.listen(PORT,()=>
 {
     console.log("Uygulama 5000 portunda dinleniyor." );
+});
+
+
+
+
+
+
+//geri bildirim
+app.use(bodyparser.urlencoded({extend:true}));
+
+app.post("/",function(req,res){
+    const comn=req.body.message;
+    const na=req.body.name;
+
+var transpoter= nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:'proje1503@gmail.com',
+        pass:'uafe ozbq vcop uhpm'
+
+    }
+
+    });
+
+
+var mailOptions={
+    from:'proje1503@gmail.com',
+    to:req.body.email,
+    cc:'proje1503@gmail.com',
+    subject:'Geribildiriminiz için teşekkürler '+ na,
+    text: 'Mesajınız--> '+comn
+
+
+};
+
+transpoter.sendMail(mailOptions,function(error,info){
+if(error){
+    console.log(error);
+}else{
+    res.redirect('/');
+    console.log('email sent'+info.response);
+}
+
+})
+
 });
 
